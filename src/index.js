da2f5fb81ac697ec133ceab7a560dc38910f22b8
aborisenok <lesha_borisenok@mail.ru>
2018-09-21 12:07:24 +0300

    one more test

--- src/index.js
+++ src/index.js
@@ -1,35 +1,65 @@
 module.exports = function count(s, pairs) {
-
-  if(s.length > 3 || pairs.length > 8) return 0;
-
-  var N = 1;
-  var sArr = s.split("");
+  if(s.length > 5 || pairs[0][1].length > 3) return 0;
   
+ 
+  var Ns = [];
+  var sArr = s.split("");
 
-  for(let i = 0; i < pairs.length; i++){
-    N *= (Math.pow(pairs[i][0], pairs[i][1]))
-  }
-
-  var count = 0;
-  for(let i = 0; i < N; i++){
-    var p = 0;
-    for(let j = 0; j < sArr.length; j++){
-      if(sArr[j] === "0"){
-        if(findCommonDiver(N, i+j) !== 1){
-          p++;
-        }
-      } 
-      if(sArr[j] === "1"){
-        if(findCommonDiver(N, i+j) === 1){
-          p++;
-        }
+  if(sArr.includes("0")){
+    var value = 1;
+    for(let i = 0; i < pairs.length; i++){
+      if(i === 0){
+        value = pairs[i][0];
+      }else{
+        value *= pairs[i][0];
       }
     }
-    if(p == sArr.length) count++;
-      
+    Ns.push(value);
+  }else{
+    for(let i = 0; i < pairs.length; i++){
+     Ns.push(pairs[i][0]);
+    }
+  }
+
+
+  var arr = [];
+  Ns.forEach(function(item){
+    var count = 0;
+    var N = item;
+    var sArrLenth = sArr.length; 
+    for(let i = 0; i < N; i++){
+      var p = 0;
+      for(let j = 0; j < sArrLenth; j++){
+        var comDiv = findCommonDiver(N, i+j);
+        if(sArr[j] === "0"){
+          if(comDiv !== 1){
+            p++;
+          }
+        } 
+        if(sArr[j] === "1"){
+          if(comDiv === 1){
+            p++;
+          }
+        }
+      }
+      if(p == sArrLenth){
+        count++;
+      }
+    }
+   arr.push(count);
+}); 
+  
+  var result = 1; 
+  for(let i = 0; i < arr.length; i++){
+    result *= arr[i];
   }
   
-  const res = count % 1000000007;
+  for(let i = 0; i < pairs.length; i++){
+    result *= Math.pow(pairs[i][0], pairs[i][1] - 1);
+  }
+
+  
+  const res = result % 1000000007;
   return res;
 }
 
